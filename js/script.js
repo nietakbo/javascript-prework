c8f0c2bac13d24d7b9c0d1ddf8bd9e9e7aa7f1ed
Julia Nowak <julianowak077@gmail.com>
2023-10-30 10:38:18 +0100

    add playerMove code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber==2){
+    computerMove='papier';
+}
+else if(randomNumber==3){
+    computerMove='nożyce'
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(randomNumber==2){
+    playerMove='papier';
+}
+else if(randomNumber==3){
+    playerMove='nożyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
